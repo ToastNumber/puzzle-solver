73167b2818db96f0f5fa189ca0fc9fc9f3b1a0fb
ToastNumber <kelseyguitar55@googlemail.com>
2015-09-03 12:51:13 +0100

    Move method added

--- src/puzzlesolver/Puzzle.java
+++ src/puzzlesolver/Puzzle.java
@@ -1,7 +1,5 @@
 package puzzlesolver;
 
-import java.awt.Point;
-
 public class Puzzle {
 	private final int[][] elements;
 	private final int width;
@@ -38,36 +36,64 @@ public class Puzzle {
 		return svaret;
 	}
 
-	private Point getPositionOfSpace() {
+	private Position getPositionOfSpace() {
 		for (int row = 0; row < height; ++row) {
 			for (int col = 0; col < width; ++col) {
-				if (elements[row][col] == 0) return new Point(row, col);
+				if (elements[row][col] == 0) return new Position(row, col);
 			}
 		}
 
-		return new Point(-1, -1);
+		return new Position(-1, -1);
 	}
 
-	public boolean canMove(Move move) {
-		Point spacePos = getPositionOfSpace();
-		
+	private boolean canMove(Move move) {
+		Position spacePos = getPositionOfSpace();
+
 		if (move == Move.UP) {
-			return spacePos.y < height - 1;
+			return spacePos.row < height - 1;
 		} else if (move == Move.RIGHT) {
-			return spacePos.x > 0;
+			return spacePos.column > 0;
 		} else if (move == Move.DOWN) {
-			return spacePos.y > 0;
+			return spacePos.row > 0;
 		} else if (move == Move.LEFT) {
-			return spacePos.x < width - 1;
+			return spacePos.column < width - 1;
 		} else {
 			return false;
 		}
 	}
-	
-	public static void main(String[] args) {
-		Puzzle p = new Puzzle(4, 4, "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 0");
-		System.out.println(p);
 
+	private void swap(Position a, Position b) {
+		int temp = elements[a.row][a.column];
+		elements[a.row][a.column] = elements[b.row][b.column];
+		elements[b.row][b.column] = temp;
+	}
+
+	public void move(Move move) {
+		if (!canMove(move)) throw new IllegalArgumentException("Move not allowed");
+		else {
+			Position spacePos = getPositionOfSpace();
+
+			if (move == Move.UP) {
+				Position below = new Position(spacePos.row + 1, spacePos.column);
+				swap(spacePos, below);
+			} else if (move == Move.RIGHT) {
+				Position left = new Position(spacePos.row, spacePos.column - 1);
+				swap(spacePos, left);
+			} else if (move == Move.DOWN) {
+				Position above = new Position(spacePos.row - 1, spacePos.column);
+				swap(spacePos, above);
+			} else if (move == Move.LEFT) {
+				Position right = new Position(spacePos.row, spacePos.column + 1);
+				swap(spacePos, right);
+			} else return;
+		}
+	}
+
+	public static void main(String[] args) {
+		Puzzle p = new Puzzle(4, 4, "1 2 3 0 5 6 7 4 9 10 11 8 13 14 15 12");
+		System.out.println(p);
+		System.out.println();
+		
 		System.out.println("Terminated");
 	}
 
