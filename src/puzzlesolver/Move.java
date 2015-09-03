40b2225a8258ce9bf255079bf45357d1913a029a
ToastNumber <kelseyguitar55@googlemail.com>
2015-09-03 12:54:06 +0100

    Move toString method changed to U R D L

--- src/puzzlesolver/Move.java
+++ src/puzzlesolver/Move.java
@@ -4,9 +4,9 @@ public enum Move {
 	UP, LEFT, DOWN, RIGHT;
 	
 	public String toString() {
-		if (this == UP) return "UP";
-		else if (this == LEFT) return "LEFT";
-		else if (this == DOWN) return "DOWN";
-		else return "RIGHT";
+		if (this == UP) return "U";
+		else if (this == LEFT) return "L";
+		else if (this == DOWN) return "D";
+		else return "R";
 	}
 }
